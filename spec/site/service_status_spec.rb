6a23a519337d16ceafc82be32d7a9a976555c5bf
Oleksiy Kovyrin <alexey@kovyrin.net>
2013-09-08 15:29:12 -0400

    Check for limit=0 in status urls

--- spec/site/service_status_spec.rb
+++ spec/site/service_status_spec.rb
@@ -3,13 +3,16 @@ require "spec_helper"
 describe NagiosHarder::Site do
   describe "#service_status method" do
     let(:site) { NagiosHarder::Site.new("http://test.com/cgi-bin/nagios3", "test-user", "test-pass") }
+    let(:fixture_response) { { :status => 200, :body => nagios_fixture('status_670_services.html'), :headers => {} } }
 
-    before do
-      stub_request(:get, "http://test-user:test-pass@test.com/cgi-bin/nagios3/status.cgi?embedded=1&noheader=1&servicegroup=all&style=detail").
-        to_return(:status => 200, :body => nagios_fixture('status_670_services.html'), :headers => {})
+    it "should explicitly set the result limit to 0" do
+      stub_request(:get, /test.com/).to_return(fixture_response)
+      site.service_status
+      WebMock.should have_requested(:get, /test.com/).with(:query => hash_including('limit' => '0'))
     end
 
     it "should return an array of statuses" do
+      stub_request(:get, /test.com/).to_return(fixture_response)
       site.service_status.should be_kind_of(Array)
       site.service_status.should have(670).services
     end
